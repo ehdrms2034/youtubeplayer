fc72ff4cd7f22fbb894c03af8c3d089240a16973
KimDongGeun <ehdrms2034@naver.com>
2019-08-19 19:24:29 +0900

    first upload

---
+++ youtubeplayer/app/src/main/java/com/example/youtubeplayer/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.youtubeplayer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        viewYoutube.initialize("AIzaSyA6L-XrMkC8RFWuE2XxweYKXJ_xu_RgWPA",this)
+
+    }
+
+    override fun onInitializationSuccess(p0: YouTubePlayer.Provider?, p1: YouTubePlayer?, p2: Boolean) {
+        p1!!.loadVideo("jBLc76LpfLs")
+    }
+
+    override fun onInitializationFailure(p0: YouTubePlayer.Provider?, p1: YouTubeInitializationResult?) {
+    }
+}
